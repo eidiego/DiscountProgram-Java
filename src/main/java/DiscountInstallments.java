5812667acb3a559360214c1293d736c602155ea0
Diego Rodrigues Barbosa <diegokod@gmail.com>
2022-12-18 21:56:49 -0300

    Feat: Add logic to iterate and show the due date for the next installments

--- src/main/java/DiscountInstallments.java
+++ src/main/java/DiscountInstallments.java
@@ -23,52 +23,55 @@ public class DiscountInstallments {
         double discount = 0;
         String discountInfo = " ";
 
-        // Conditional logic to define how much discount will be applied.
-        switch (installments) {
-            case 0:
-                discountInfo = "Favor escolher um número superior a ZERO";
-                break;
-            case 1:
-                discount = totalAmount * 10 / 100;
-                discountInfo = "O valor do desconto é de 10%";
-                break;
-            case 2:
-                discount = totalAmount * 8 / 100;
-                discountInfo = "O valor do desconto é de 8%";
-                break;
-            case 3:
-                discount = totalAmount * 6 / 100;
-                discountInfo = "O valor do desconto é de 10%";
-                break;
-            case 4:
-                discount = totalAmount * 4 / 100;
-                discountInfo = "O valor do desconto é de 8%";
-                break;
-            case 5:
-                discount = totalAmount * 2 / 100;
-                discountInfo = "O valor do desconto é de 10%";
-                break;
-            default:
-                if (installments > 5) {
-                    discount = totalAmount * 0 / 100;
-                    discountInfo = "O valor do desconto é de 0%";
+            // Defensive programming to prevent negative unwanted values
+            if (installments > 0  && totalAmount > 0) {
+                // Conditional logic to define how much discount will be applied.
+                switch (installments) {
+                    case 1:
+                        discount = totalAmount * 10 / 100;
+                        discountInfo = "O valor do desconto é de 10%";
+                        break;
+                    case 2:
+                        discount = totalAmount * 8 / 100;
+                        discountInfo = "O valor do desconto é de 8%";
+                        break;
+                    case 3:
+                        discount = totalAmount * 6 / 100;
+                        discountInfo = "O valor do desconto é de 10%";
+                        break;
+                    case 4:
+                        discount = totalAmount * 4 / 100;
+                        discountInfo = "O valor do desconto é de 8%";
+                        break;
+                    case 5:
+                        discount = totalAmount * 2 / 100;
+                        discountInfo = "O valor do desconto é de 10%";
+                        break;
+                    default:
+                        if (installments >= 6) {
+                            discount = totalAmount * 0 / 100;
+                            discountInfo = "O valor do desconto é de 0%";
+                        }
                 }
-        }
+                double totalAmountWithDiscount = totalAmount - discount;
 
-        double totalAmountWithDiscount = totalAmount - discount;
+                System.out.println("O valor total com o desconto aplicado é: ");
+                System.out.println(totalAmountWithDiscount);
+                System.out.println(discountInfo);
+                System.out.print("O valor das parcelas será de: ");
 
-        System.out.println("O valor total com o desconto aplicado é: ");
-        System.out.println(totalAmountWithDiscount);
-        System.out.println(discountInfo);
-        System.out.print("O valor das parcelas será de: ");
+                // Formater to show a amount without unnecessary numbers after comma
+                DecimalFormat formato = new DecimalFormat("#.##");
 
-        // Formater to show a amount without unnecessary numbers after comma
-        DecimalFormat formato = new DecimalFormat("#.##");
+                System.out.println(formato.format(totalAmountWithDiscount / installments));
+                System.out.println("As datas de vencimento das parcelas serão: ");
+                for (int month = 1; month <= installments; month++) {
+                    LocalDate dueDate = LocalDate.now().plusMonths(month);
+                    System.out.println(dueDate);
+                }
+            }else
+                System.out.println("Você precisa informar valores acima de ZERO!");
 
-        System.out.println(formato.format(totalAmountWithDiscount / installments));
-        System.out.println("As datas de vencimento das parcelas serão: ");
-        LocalDate dueDate = LocalDate.now().plusMonths(1);
-        System.out.println(dueDate);
 
         sc.close();
 
